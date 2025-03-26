8326c9e595e8cc60266c714bed3da66638e01174
Tim <tim.bjorkegren@gmail.com>
2025-03-26 13:49:29 +0100

    made a cs file with xUnit to test methods

---
+++ TestPokerMethods.cs
@@ -0,0 +1,34 @@
+using Xunit;
+
+namespace testmethods;
+
+public class MethodTester()
+{
+
+    /*
+    [Fact]
+    public void TestAdd()
+    {
+        // Arrange
+
+        // Act
+        int result = Program.Add(2, 2);
+
+        // Assert
+        Assert.Equal(4, result);
+    }
+
+    [Theory]
+    [InlineData(4)]
+    [InlineData(6)]
+    [InlineData(10)]
+    public void MyEvenTheory(int number)
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(Program.IsEven(number));
+    } */
+}
